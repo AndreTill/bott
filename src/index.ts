0e6545d088e7ebd645e5358b5d0f45c199387b13
root <root@vps.andretill.ru>
2025-08-01 22:47:38 +0300

    lists

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { message } from 'telegraf/filters'
 import * as fs from 'fs';
 import * as path from 'path';
 import { format } from 'date-fns';
-import { ru } from 'date-fns/locale';
+import { de, hi, ru } from 'date-fns/locale';
 import { Buffer } from 'buffer';
 
 import { VK } from 'vk-io';
@@ -17,6 +17,7 @@ import { runInContext } from 'vm';
 import { stringify } from 'querystring';
 
 import * as env from 'dotenv';
+import { diff } from 'util';
 
 
 
@@ -45,16 +46,12 @@ var debug_mode: Boolean = (envv.parsed.DEBUG === 'true') ? true : false;
 //     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 //     );`;    
 
-  interface History {
-    friends: number;
-    groups: number;
-    followers: number;
-    subscriptions: number;
-    photos: number;
-    videos: number;
+
+  interface History implements VK.types.User {
+    friends: [ VK.types.User ];
   };
 
-  var hist: History
+  var hist: History;
 //    = {
 //     friends: 0,
 //     groups: 0,
@@ -118,65 +115,82 @@ function resolve_dev(input: any): string {
     return ret
 }
 
+function debug_log(data: any, from: string! ) {
+    if (debug_mode) {
+        console.log("\n\n");
+        console.log(' >> DEBUG LOG: ');
+        console.log(' >> From: ', from);
+        console.log(data);
+        console.log(" END OF DEBUG LOG \n\n");
+    }
+}
+
 async function run(): Promise<string> {
     var res = ""
     await vk.api.users.get({
         user_ids: [ 'katya_bach' ],
-        fields: [ 'online' , 'online_info', 'last_seen', 'followers_count', 'counters' ]
-    }).then((response) => {      
-            res += "   [ ::: VK ::: " + response[0]['first_name'] + ' ' + response[0]['last_name'] + "](https://vk.com/id" + response[0]['id'] + ") с " + resolve_dev(response[0]["last_seen"]['platform']) + " ";
-            res += `\n   ${response[0]['online'] ? 'Online' : 'Offline'} `;
-            res += `\n   ${format(new Date((response[0].last_seen.time * 1000 - 7200)), 'dd MMMM yyyy в HH:mm',{ locale: ru })} `;
+        fields: [ 'online' , 'online_info', 'last_seen', 'followers_count', 'counters', 'lists' ]
+    }).then((response) => {
+            res += "[ ::: VK ::: " + response[0]['first_name'] + ' ' + response[0]['last_name'] + "](https://vk.com/id" + response[0]['id'] + " ) ";
+            res += "\n ```";
+            res += `\n | Cтатус     \t| ${response[0]['online'] ? 'Online' : 'Offline'} `;
+            res += `\n | Заходил    \t| ${format(new Date((response[0].last_seen.time * 1000 - 7200)), 'dd MMMM yyyy в HH:mm',{ locale: ru })} `;
+            res += `\n | Устройство \t| ${resolve_dev(response[0]["last_seen"]['platform'])} `;
+            res += "\n ```";
             if (hist) {
-                if (hist.friends !== (response[0]['followers_count'] - response[0]['counters']['followers'])) res += `\n   ${response[0]['followers_count'] - response[0]['counters']['followers'] - hist.friends} новых друзей `;
-                if (hist.groups !== response[0]['counters']['pages']) res += `\n   ${response[0]['counters']['pages'] - hist.groups} новых групп `;
-                if (hist.followers !== response[0]['counters']['followers']) res += `\n   ${response[0]['counters']['followers'] - hist.followers} новых подписчиков `;
-                if (hist.subscriptions !== response[0]['counters']['subscriptions']) res += `\n   ${response[0]['counters']['subscriptions'] - hist.subscriptions} новых подписок `;
-                if (hist.photos !== response[0]['counters']['photos']) res += `\n   ${response[0]['counters']['photos'] - hist.photos} новых фотографий `;
-            } else {
-                hist = {
-                    friends: 0,
-                    groups: 0,
-                    followers: 0,
-                    subscriptions: 0,
-                    photos: 0,
-                    videos: 0
-                };
+                if ((hist.followers_count - hist.counters.followers) !== (response[0]['followers_count'] - response[0]['counters']['followers'])) {
+                    res += `\n   ${response[0]['followers_count'] - response[0]['counters']['followers'] - (hist.followers_count - hist.counters.followers)} новых друзей `;
+                }
+                if (hist.counters.pages !== response[0]['counters']['pages']) res += `\n   ${response[0]['counters']['pages'] - hist.counters.pages} новых групп `;
+                if (hist.counters.followers !== response[0]['counters']['followers']) res += `\n   ${response[0]['counters']['followers'] - hist.counters.followers} новых подписчиков `;
+                if (hist.counters.subscriptions !== response[0]['counters']['subscriptions']) res += `\n   ${response[0]['counters']['subscriptions'] - hist.counters.subscriptions} новых подписок `;
+                if (hist.counters.photos !== response[0]['counters']['photos']) res += `\n   ${response[0]['counters']['photos'] - hist.counters.photos} новых фотографий `;
             };
 
-            hist.friends = (response[0]['followers_count'] - response[0]['counters']['followers']);
-            hist.groups = response[0]['counters']['pages'];
-            hist.followers = response[0]['counters']['followers'];
-            hist.subscriptions = response[0]['counters']['subscriptions'];
-            hist.photos = response[0]['counters']['photos'];
-            hist.videos = response[0]['counters']['videos'];
-            console.log(hist);
+            hist = response[0] as VK.types.User;
+            
+    debug_log(hist, "hist");
     //     db.run(`INSERT OR REPLACE INTO users (user_id, friends, groups, followers, subscriptions, photos, videos, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
     //         [response[0]['id'], (response[0]['followers_count'] - response[0]['counters']['followers']), response[0]['counters']['pages'], response[0]['counters']['followers'], response[0]['counters']['subscriptions'], response[0]['counters']['photos'], response[0]['counters']['videos'], new Date()],
     //         (err) => {
     //             if (err) {
-    //             console.error('Error inserting or updating user data:', err.message);
-    //         } else {
-    //             console.log('User data inserted or updated successfully.');
+    //                 console.error('Error inserting or updating user data:', err.message);
+    //             } else {
+    //                 console.log('User data inserted or updated successfully.');
+    //             }
     //         }
-    //     }
     // );
 
-            console.log('User data:', response[0]);
+     //debug_log(response[0], "User data");
 
+    // if (debug_mode) {
+    //     res += "\n\n ```DEBUG ";
+    //     res += `\n FROM: response[0] `;
+    //    res += `\n ${(response[0]['followers_count'] - response[0]['counters']['followers'])} друзей, ${hist.friends} было `;
+    //    res += `\n ${response[0]['counters']['pages']} групп, ${hist.groups} было `;
+    //    res += `\n ${response[0]['counters']['followers']} подписчиков, ${hist.followers} было `;
+    //    res += `\n ${response[0]['counters']['subscriptions']} подписок, ${hist.subscriptions} было `;
+    //    res += `\n ${response[0]['counters']['photos']} фото, ${hist.photos} было `;
+    //    res += `\n ${response[0]['counters']['videos']} видео, ${hist.videos} было `;
+    //     res += "\n END OF DEBUG INFO ```";
 
+    
     if (debug_mode) {
-        res += `\n DEBUG INFO `;
-       res += `\n ${(response[0]['followers_count'] - response[0]['counters']['followers'])} друзей, ${hist.friends} было `;
-       res += `\n ${response[0]['counters']['pages']} групп, ${hist.groups} было `;
-       res += `\n ${response[0]['counters']['followers']} подписчиков, ${hist.followers} было `;
-       res += `\n ${response[0]['counters']['subscriptions']} подписок, ${hist.subscriptions} было `;
-       res += `\n ${response[0]['counters']['photos']} фото, ${hist.photos} было `;
-       res += `\n ${response[0]['counters']['videos']} видео, ${hist.videos} было `;
-        res += `\n END OF DEBUG INFO `;
+        res += "\n\n ```DEBUG ";
+       res += "\n | Списки      	| Значение 	| Было \t	|";
+       res += "\n |-------------	|----------	|------	\t|";
+       res += `\n | Друзей      	| \t ${response[0]['followers_count'] - response[0]['counters']['followers']} \t\t | ${hist.followers_count - hist.counters.followers} \t |`;
+       res += `\n | Групп       	| \t ${response[0]['counters']['pages']} \t\t\t | ${hist.counters.pages} \t\t |`;
+       res += `\n | Подписок    	| \t ${response[0]['counters']['subscriptions']} \t\t\t\t | ${hist.counters.subscriptions} \t\t\t |`;
+       res += `\n | Подписчиков 	| \t ${response[0]['counters']['followers']} \t\t\t | ${hist.counters.followers} \t\t |`;
+       res += `\n | Фотографий  	| \t ${response[0]['counters']['photos']} \t\t\t | ${hist.counters.photos} \t\t |`;
+       res += `\n | Видео       	| \t ${response[0]['counters']['videos']} \t\t\t | ${hist.counters.videos} \t\t |`;
+       res += "\n ```";
+    };
 
-    }
-    })
+
+    });
+
 
 
     return res;
@@ -219,8 +233,8 @@ function filesarray(path: string) {
 
     const filespath = files.map(item => `${path}/${item}`)
 
-    if (debug_mode) console.log(...filespath.slice(0,10), ' ... ')
-    return filespath
+    debug_log(filespath.slice(0, 10).join(', ') + " ... ", "files scanned");
+    return filespath;
 }
 
 var step = 0;
@@ -235,7 +249,25 @@ bot.command('status', async (ctx) => {
 });
 });
 
+function getSymmetricDifference<T>(arr1: T[], arr2: T[]): T[] {
+  const uniqueToA = arr1.filter(item => !arr2.includes(item));
+  const uniqueToB = arr2.filter(item => !arr1.includes(item));
+  return [...uniqueToA, ...uniqueToB];
+}
 
+
+function list_updates(resp: VK.types.User, list: string): string {
+    var res = "";
+    if (hist) {
+        res += `\n   ${hist.followers_count - hist.counters.followers} новых друзей `;
+        res += `\n   ${hist.counters.pages} новых групп `;
+        res += `\n   ${hist.counters.followers} новых подписчиков `;
+        res += `\n   ${hist.counters.subscriptions} новых подписок `;
+        res += `\n   ${hist.counters.photos} новых фотографий `;
+        res += `\n   ${hist.counters.videos} новых видео `;
+    }
+    return res;
+}
 
 bot.command('photos', async (ctx) => {
     var fls = [];
@@ -333,7 +365,7 @@ bot.on(message('photo'), async (ctx) => {
             const destPath = path.join('./img', `${fileId}.jpg`);
             const https = require('https');
             const file = fs.createWriteStream(destPath);
-            console.log(fileUrl)
+            debug_log(fileUrl, "fileUrl for photo");
             https.get(fileUrl, (response) => {
                 response.pipe(file);
                 file.on('finish', () => {
@@ -359,7 +391,7 @@ bot.on(message('video'), async (ctx) => {
             const destPath = path.join('./vid', `${photo.file_name}`);
             const https = require('https');
             const file = fs.createWriteStream(destPath);
-            console.log(fileUrl)
+            debug_log(fileUrl, "fileUrl for video");
             https.get(fileUrl, (response) => {
                 response.pipe(file);
                 file.on('finish', () => {
